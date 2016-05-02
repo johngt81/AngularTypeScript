bf54fa8862bd31e4a8efb6476853e8fe0c43de59
jcyovera <jcyovera@gmail.com>
2016-05-01 23:16:30 -0500

    Creating package json Adding bootstrap Including routes for modules

--- firstdemo/app/app.js
+++ firstdemo/app/app.js
@@ -1,6 +1,16 @@
 var moduleJC;
 (function (moduleJC) {
-    'use strict';
+    'user strict';
     var demoModule = angular.module('demoModule', ['ngRoute']);
+    demoModule.config(demoRouteConfig);
+    function demoRouteConfig($routeProvider) {
+        $routeProvider.when('/inicio', {
+            templateUrl: 'app/views/inicio.html'
+        }).when('/personajes', {
+            templateUrl: 'app/views/personajes.html'
+        }).when('/acerca', {
+            templateUrl: 'app/views/acerca.html'
+        }).otherwise('/inicio');
+    }
 })(moduleJC || (moduleJC = {}));
 //# sourceMappingURL=app.js.map
--- firstdemo/app/app.ts
+++ firstdemo/app/app.ts
@@ -1,6 +1,17 @@
 namespace moduleJC{
-    'use strict'
-    var demoModule=angular.module('demoModule',['ngRoute']);
-    
+    'user strict'
+    var demoModule= angular.module('demoModule',['ngRoute']);
+    demoModule.config(demoRouteConfig);
+    function demoRouteConfig($routeProvider:ng.route.IRouteProvider):void{
+        
+        $routeProvider.when('/inicio',{
+            templateUrl:'app/views/inicio.html'
+        }).when('/personajes',{
+            templateUrl:'app/views/personajes.html'
+        }).when('/acerca',{
+            templateUrl:'app/views/acerca.html'
+        }).otherwise('/inicio');
+        
+    }
     
 }
